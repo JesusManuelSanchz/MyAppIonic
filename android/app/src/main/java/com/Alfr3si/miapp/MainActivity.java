6d553d6d817e1f52530b7042baad1c50d721b959
JesusManuelSanchz <chuchopro76@gmail.com>
2026-04-22 14:50:12 -0600

    first commit

---
+++ android/app/src/main/java/com/Alfr3si/miapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.Alfr3si.miapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Alfr3si.miapp',
+  appName: 'MiAppPersonal',
+  webDir: 'www'
+};
+
+export default config;
